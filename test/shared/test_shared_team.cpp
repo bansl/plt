92f72cd0e3de54678f22be0b8a353f428714a1bd
Ben <benjamin.anslot2@gmail.com>
2019-10-23 22:56:47 +0200

    fix shared team test with new test class

--- test/shared/test_shared_team.cpp
+++ test/shared/test_shared_team.cpp
@@ -4,23 +4,11 @@
 
 using namespace state;
 
-// BOOST_AUTO_TEST_CASE(TestTeam)
-// {
-// 	Team testTeam {};
-// 	testTeam.characterNumber=0;
-// 	testTeam.listCharacter={};
-// 	BOOST_CHECK_EQUAL(testTeam.characterNumber,testTeam.listCharacter.size());
-// }
+BOOST_AUTO_TEST_CASE(TestTeam)
+{
+	Team testTeam ;
+    BOOST_CHECK_EQUAL(testTeam.getListCharacter().size(),0);
+    testTeam.addCharacter();
+	BOOST_CHECK_EQUAL(testTeam.getListCharacter().size(),1);
+}
 
-// BOOST_AUTO_TEST_CASE(TestHealItem)
-// {
-//   Item exheal {  };
-//   exheal.name="potion";
-//   exheal.quantity=2;
-//   // exheal.healthRestoreAmount=42;
-
-//   Team player1 {};
-//   player1.listItems.push_back(exheal);
-
-//   BOOST_CHECK_EQUAL((player1.getItems())[0].quantity,2);
-// }
