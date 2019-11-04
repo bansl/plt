8eea7f6038084f2528caada649ef145f1b22a257
Mathieu <mathieu.hadjadji@ensea.fr>
2019-11-04 12:16:30 +0100

    update UseSkill

--- src/shared/engine/UseSkill.cpp
+++ src/shared/engine/UseSkill.cpp
@@ -6,6 +6,15 @@ using namespace engine;
 using namespace state;
 using namespace std;
 
+bool UseSkill::action(state::Turn& turn){
+  if((int)character.getSkillList(character.getJob(),character.getLevel()).size()<skillNumber){
+    return true;
+  }
+  else{
+    return false;
+  }
+}
+
 UseSkill::UseSkill(state::Character& usedCharacter,std::vector<std::unique_ptr<state::Character>>& targetCharacter,int numberSkill):character(usedCharacter),targetList(targetCharacter),skillNumber(numberSkill){
   commandType= UseSkillcmd;
 }
