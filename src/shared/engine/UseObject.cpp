cccf38ad38a12c4af58387b0003dda59b9c48fdb
Mathieu <mathieu.hadjadji@ensea.fr>
2019-11-05 09:15:23 +0100

    update useskill and useobject

--- src/shared/engine/UseObject.cpp
+++ src/shared/engine/UseObject.cpp
@@ -3,16 +3,22 @@
 using namespace engine;
 using namespace state;
 
-UseObject::UseObject (state::Character& targetCharacter, state::Item object):targetCharacter(targetCharacter),object(object){
+UseObject::UseObject (state::Character& targetCharacter, int usedObject,int numberTeam):character(targetCharacter),object(usedObject),teamNumber(numberTeam){
     commandType=UseObjectcmd;
 }
 
 
 bool UseObject::action(state::Turn& turn){
+  turn.getTeams()[teamNumber]->getItems()[object].quantity+=-1;
+  character.setCurrentHP(turn.getTeams()[teamNumber]->getItems()[object].getConsumable().healthRestoreAmount);
+  character.setCurrentMP(turn.getTeams()[teamNumber]->getItems()[object].getConsumable().manaRestoreAmount);
   return true;
 }
 
 
 bool UseObject::validate(state::Turn& turn){
-  return true;
+  if(turn.getTeams()[teamNumber]->getItems()[object].quantity>0){
+    return true;
+  }
+  return false;
 }
--- src/shared/engine/UseSkill.cpp
+++ src/shared/engine/UseSkill.cpp
@@ -7,14 +7,22 @@ using namespace state;
 using namespace std;
 
 bool UseSkill::action(state::Turn& turn){
-  if((int)character.getSkillList(character.getJob(),character.getLevel()).size()<skillNumber){
-    return true;
+  for(int i=0;i<(int)targetList.size();i++){
+    targetList[i]->setCurrentHP(character.getSkillList(character.getJob(),character.getLevel())[skillNumber].hpRecovery-character.getSkillList(character.getJob(),character.getLevel())[skillNumber].damage);
   }
-  else{
-    return false;
-  }
+  character.setCurrentMP(-character.getSkillList(character.getJob(),character.getLevel())[skillNumber].mpCost);
+  return true;
 }
 
 UseSkill::UseSkill(state::Character& usedCharacter,std::vector<std::unique_ptr<state::Character>>& targetCharacter,int numberSkill):character(usedCharacter),targetList(targetCharacter),skillNumber(numberSkill){
   commandType= UseSkillcmd;
+}
+
+bool UseSkill::validate(state::Turn& turn){
+  if((int)character.getSkillList(character.getJob(),character.getLevel()).size()<skillNumber){
+    if(character.getCurrentMP()>=character.getSkillList(character.getJob(),character.getLevel())[skillNumber].mpCost){
+      return true;
+    }
+  }
+  return false;
 }
