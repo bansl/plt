c22bdc1df72f99a05f255ed644485ecf01e05b19
Ben <benjamin.anslot2@gmail.com>
2019-12-13 14:13:50 +0100

    temp fix to preserve deep_ai commands

--- src/shared/ai/DeepAI.cpp
+++ src/shared/ai/DeepAI.cpp
@@ -1,6 +1,7 @@
 #include "../ai.h"
 #include "../engine.h"
 #include <iostream>
+#include <queue>
 using namespace std;
 using namespace ai;
 using namespace engine;
@@ -57,14 +58,18 @@ void DeepAI::deepCommandList(Engine& engine,int teamNumber){
   cout<<"DeepAi: Team Number "<<teamNumber<<" is playing"<<endl;
   int k=0;
   bool hasNotMoved=true;
-	std::vector<std::unique_ptr<Command>> commandsTemp;
+
+  std::vector<CommandList> CommandtypeTemp;
   while(k<(int)engine.getTurn().getTeams()[teamNumber]->getListCharacter().size()){
+    for (size_t k = 0; k < 5; k++) InstructionAdded[k]=false;
     if(hasNotMoved){
       cout<<"Character Number "<<k<<" is playing"<<endl;
     }
     int maxScore=-500;
     UseSkill commandnull(*engine.getTurn().getTeams()[teamNumber]->getListCharacter()[k],*engine.getTurn().getTeams()[teamNumber]->getListCharacter()[k],500,0);
     unique_ptr<Command> ptr_command (new UseSkill(commandnull));
+    CommandtypeTemp.push_back({});
+    
 
     cout<<"Start Attack scoring"<<endl;
     for(int t=0;t<(int)engine.getTurn().getTeams().size();t++){
@@ -80,12 +85,12 @@ void DeepAI::deepCommandList(Engine& engine,int teamNumber){
               maxScore=minMaxScore;
               // cout<<"New MaxScore: "<<maxScore<<endl;
               ptr_command.reset(new Attack(attacktest));
-            }
-						for (int i=0;i<(int)commandsTemp.size();i++){
-							commandsTemp[i]->validate(engine.getTurn());
-							engine.addCommand(move(commandsTemp[i]));
-						}
+              Attackqueue.push(attacktest);
+              InstructionAdded[0]=true;
+              CommandtypeTemp.back()=Attackcmd;
 
+            }
+						restoreCommandList();
           }
         }
       }
@@ -99,7 +104,7 @@ void DeepAI::deepCommandList(Engine& engine,int teamNumber){
       int tempY=engine.getTurn().getTeams()[teamNumber]->getListCharacter()[k]->getPosition().getY();
         for(int x=max((int)engine.getTurn().getTeams()[teamNumber]->getListCharacter()[k]->getPosition().getX()-5,0);x<min((int)engine.getTurn().getTeams()[teamNumber]->getListCharacter()[k]->getPosition().getX()+5,(int)engine.getTurn().getMap().size());x+=2){
           for(int y=max(0,(int)engine.getTurn().getTeams()[teamNumber]->getListCharacter()[k]->getPosition().getY()-5);y<min((int)engine.getTurn().getTeams()[teamNumber]->getListCharacter()[k]->getPosition().getY()+5,(int)engine.getTurn().getMap().size());y+=2){
-            cout<< "x: " << x << ", y: " << y << endl;
+            // cout<< "x: " << x << ", y: " << y << endl;
             Position dest;
             dest.setPos(x,y);
             Move movetest(*engine.getTurn().getTeams()[teamNumber]->getListCharacter()[k], dest);
@@ -116,11 +121,12 @@ void DeepAI::deepCommandList(Engine& engine,int teamNumber){
                 maxScore=minMaxScore;
                 // cout<<"New MaxScore: "<<maxScore<<endl;
                 ptr_command.reset(new Move (movetest));
+                updateQueue();
+                Movequeue.push(movetest);
+                InstructionAdded[1]=true;
+                CommandtypeTemp.back()=Movecmd;
               }
-							for (int i=0;i<(int)commandsTemp.size();i++){
-								commandsTemp[i]->validate(engine.getTurn());
-								engine.addCommand(move(commandsTemp[i]));
-							}
+							restoreCommandList();
 
             }
           }
@@ -139,11 +145,12 @@ void DeepAI::deepCommandList(Engine& engine,int teamNumber){
         maxScore=minMaxScore;
         // cout<<"New MaxScore: "<<maxScore<<endl;
         ptr_command.reset(new Defend (deftest));
+        updateQueue();
+        Defendqueue.push(deftest);
+        InstructionAdded[2]=true;
+        CommandtypeTemp.back()=Defendcmd;
       }
-			for (int i=0;i<(int)commandsTemp.size();i++){
-				commandsTemp[i]->validate(engine.getTurn());
-				engine.addCommand(move(commandsTemp[i]));
-			}
+			restoreCommandList();
 
     }
     cout<<"End Defend scoring"<<endl;
@@ -161,11 +168,12 @@ void DeepAI::deepCommandList(Engine& engine,int teamNumber){
             maxScore=minMaxScore;
             // cout<<"New MaxScore: "<<maxScore<<endl;
   				  ptr_command.reset(new UseObject(testUseObject));
+            updateQueue();
+            UseObjectqueue.push(testUseObject);
+            InstructionAdded[3]=true;
+            CommandtypeTemp.back()=UseObjectcmd;
           }
-					for (int i=0;i<(int)commandsTemp.size();i++){
-						commandsTemp[i]->validate(engine.getTurn());
-						engine.addCommand(move(commandsTemp[i]));
-					}
+					restoreCommandList();
 
         }
       }
@@ -187,11 +195,12 @@ void DeepAI::deepCommandList(Engine& engine,int teamNumber){
                     maxScore=minMaxScore;
                     // cout<<"New MaxScore: "<<maxScore<<endl;
                     ptr_command.reset(new UseSkill(testUseSkill));
+                    updateQueue();
+                    UseSkillqueue.push(testUseSkill);
+                    InstructionAdded[4]=true;
+                    CommandtypeTemp.back()=UseSkillcmd;
                   }
-									for (int i=0;i<(int)commandsTemp.size();i++){
-										commandsTemp[i]->validate(engine.getTurn());
-										engine.addCommand(move(commandsTemp[i]));
-									}
+									restoreCommandList();
 
 	            }
 	          }
@@ -203,6 +212,7 @@ void DeepAI::deepCommandList(Engine& engine,int teamNumber){
     if(ptr_command->validate(engine.getTurn())){
       if (ptr_command->commandType==Attackcmd){
 				cout << "->attack instruction added " << endl;
+        
       }
       else if (ptr_command->commandType==Movecmd){
         cout << "->move instruction added " << endl;
@@ -223,8 +233,7 @@ void DeepAI::deepCommandList(Engine& engine,int teamNumber){
       else{
         hasNotMoved=false;
       }
-			commandsTemp.push_back(move(ptr_command));
-	    engine.addCommand(move(commandsTemp[commandsTemp.size()-1]));
+	    engine.addCommand(move(ptr_command));
     }
     else{
       EndTurn endturntest(teamNumber);
@@ -235,5 +244,62 @@ void DeepAI::deepCommandList(Engine& engine,int teamNumber){
         cout << "->endturn instruction added " << endl;
       }
     }
+  }
+}
+
+
+void DeepAI::restoreCommandList(){
+for (int i=0;i<(int)CommandtypeList.size();i++){
+    unique_ptr<Command> ptr_commandtemp;
+    if (CommandtypeList[i]==Attackcmd){
+      ptr_commandtemp.reset(new Attack(Attackqueue.front()));
+      Attackqueue.push(Attackqueue.front());
+      Attackqueue.pop();
+    }
+    else if (CommandtypeList[i]==Movecmd){
+      ptr_commandtemp.reset(new Move(Movequeue.front()));
+      Movequeue.push(Movequeue.front());
+      Movequeue.pop();
+    }
+    else if (CommandtypeList[i]==Defendcmd){
+      ptr_commandtemp.reset(new Defend(Defendqueue.front()));
+      Defendqueue.push(Defendqueue.front());
+      Defendqueue.pop();
+    }
+    else if (CommandtypeList[i]==UseObjectcmd){
+      ptr_commandtemp.reset(new UseObject(UseObjectqueue.front()));
+      UseObjectqueue.push(UseObjectqueue.front());
+      UseObjectqueue.pop();
+    }
+    else if (CommandtypeList[i]==UseSkillcmd){
+      ptr_commandtemp.reset(new UseSkill(UseSkillqueue.front()));
+      UseSkillqueue.push(UseSkillqueue.front());
+      UseSkillqueue.pop();
+    }
+    ptr_commandtemp->validate(engine.getTurn());
+    engine.addCommand(move(ptr_commandtemp));
+  }
+}
+
+void DeepAI::updateQueue(){
+  if (InstructionAdded[0]) {
+    Attackqueue.pop();
+    InstructionAdded[0]=false;
+  }
+  if (InstructionAdded[1]) {
+    Movequeue.pop();
+    InstructionAdded[1]=false;
+  }
+  if (InstructionAdded[2]) {
+    Defendqueue.pop();
+    InstructionAdded[2]=false;
+  }
+  if (InstructionAdded[3]) {
+    UseObjectqueue.pop();
+    InstructionAdded[3]=false;
+  }
+  if (InstructionAdded[4]) {
+    UseSkillqueue.pop();
+    InstructionAdded[4]=false;
   }
 }
